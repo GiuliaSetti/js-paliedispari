ddc3a56d3703c932514a6f362befb407c41d5982
GiuliaSetti <giulia.setti96@gmail.com>
2023-03-02 15:35:36 +0100

    scaffolding + pseudocode palindroma

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// Palidroma
+// Chiedere all’utente di inserire una parola
+// Creare una funzione per capire se la parola inserita è palindroma
+
+
+
+/* 
+- trascrivo gli elementi HTML in JS.
+
+- chiedo all'utente una parola
+al CLICK del bottone:
+
+    ? SE la parola è palindroma
+        ° stampo in pagina che la parola è palindroma.
+
+    : ALTRIMENTI
+        ° stampo in pagina che la parola non è palindroma. 
+
+*/
